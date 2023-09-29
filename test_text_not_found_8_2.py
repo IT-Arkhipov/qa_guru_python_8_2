f92b712f451c5fa78b1c86dcf5a129b5876fc804
IT-Arkhipov <it.arkhipov@gmail.com>
2023-09-29 12:13:26 +0300

    test for random text in Google search with conftest.py fixture

---
+++ test_text_not_found_8_2.py
@@ -0,0 +1,14 @@
+from selene import be, have
+from selene.support.shared import browser
+import random
+import string
+import pytest
+
+
+def test_random_text_not_found_in_google():
+    browser.open('https://google.com')
+    random_text = ''.join(random.choices(string.ascii_letters + string.digits, k=50))
+    browser.element('[name="q"]').should(be.blank).type(random_text).press_enter()
+    asserted_text = "ничего не найдено"
+    assert browser.element('[class="card-section"]').matching(have.text(asserted_text)), \
+        f"На случайный запрос '{random_text}' результат поиска не содержит текст '{asserted_text}'"
